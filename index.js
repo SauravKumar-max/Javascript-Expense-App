15bdab0c6fb756da4825fa3792b20b17fa124131
saurav-max <sauravmax237@gmail.com>
2020-11-11 21:58:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const expense = document.querySelector(".expense");
+const amount = document.querySelector(".amount");
+const add = document.querySelector(".add");
+const result = document.querySelector(".result");
+const allExpenses = document.querySelector(".all-expenses");
+
+let totalAmount = 0;
+let i = 0;
+const array = [];
+
+const expenseApp = () => {
+  if((expense.value != "") && (amount.value != "")){
+  const expenseObj = {};
+  const expenseEl = expense.value;
+  const amountEl= parseInt(amount.value);
+  
+  expenseObj.desc = expenseEl;
+  expenseObj.amt = amountEl; 
+  expenseObj.classname = `cross-${i++}`;
+  array.push(expenseObj);
+ 
+   const arrayObj = array.map(items => {
+     return `<div class="description">
+                ${items.desc} : ${items.amt}
+                <button class=${items.classname}> X </button>
+              </div>`});
+ 
+              
+  const arrayObjJoin = arrayObj.join('');
+  allExpenses.innerHTML = arrayObjJoin;  
+  totalAmount += amountEl;
+  result.textContent = "Total:" + totalAmount;
+  
+  expense.value = "";
+  amount.value = "";
+    } 
+  }  
+ 
+ 
+ 
+ const deleteApp = (e) =>{
+   let some = e.target.className;
+    array.forEach((lists) =>{
+       if(lists.classname === some){
+          totalAmount = totalAmount - lists.amt;
+          result.textContent = "Total:" + totalAmount;
+      
+      const arrayIndex = array.findIndex((x)=> x.classname === some);
+      array.splice(arrayIndex, 1);
+      
+      const arrayFiltered = array.map(items =>{
+       return `<div class="description">
+                ${items.desc} : ${items.amt}
+                <button class=${items.classname}> X </button>
+              </div>`;
+     });
+      
+      const arrayFilteredJoin = arrayFiltered.join('');
+      allExpenses.innerHTML= arrayFilteredJoin;
+      
+      }     
+  });
+  
+       
+  
+} 
+ 
+allExpenses.addEventListener('click', deleteApp);
+add.addEventListener('click', expenseApp);
